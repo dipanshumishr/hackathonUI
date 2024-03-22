46ac6f76a6fc56a175f20ac09a666bb85bd9cb6e
Dipanshu Mishra <dipanshu.mishra@vegrow.in>
2024-03-22 14:15:25 +0530

    changes in css according to requirement

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import vegrowIcon from './vegrow.png'; // Import the Vegrow icon
 
-
-const Navbar = ({ onChangeTab }) => {
-  const [activeTab, setActiveTab] = useState(0);
-
-  const handleTabClick = (tabIndex) => {
-    setActiveTab(tabIndex);
-    onChangeTab(tabIndex);
-  };
-
+const Navbar = () => {
   return (
-    <div className="navbar">
-      <div
-        className={`tab ${activeTab === 0 ? 'active' : ''}`}
-        onClick={() => handleTabClick(0)}
-      >
-        Tab 1
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <img src={vegrowIcon} alt="Vegrow Icon" className="navbar-icon" />
+        <span className="navbar-text">PURCHASE ORDER TRAIL</span>
       </div>
-      <div
-        className={`tab ${activeTab === 1 ? 'active' : ''}`}
-        onClick={() => handleTabClick(1)}
-      >
-        Tab 2
-      </div>
-      <div
-        className={`tab ${activeTab === 2 ? 'active' : ''}`}
-        onClick={() => handleTabClick(2)}
-      >
-        Tab 3
-      </div>
-      {/* Add more tabs as needed */}
-    </div>
+    </nav>
   );
 };
 
